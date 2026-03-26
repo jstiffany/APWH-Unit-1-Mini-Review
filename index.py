c554b21398b034a8772cae13a34d647a3fbcdbfb
Tiffany <191784353+jstiffany@users.noreply.github.com>
2026-03-26 22:31:55 +0000

    first update before i get on my pc to...do stuff i guess

---
+++ index.py
@@ -0,0 +1,20 @@
+## TITLE & INTRO ##
+
+print("-- AP WORLD HISTORY --")
+print("UNIT 1 PRACTICE TEST\n")
+print("This practice test has 5 questions\nand the answers will be shown\nthroughout the quiz.")
+print('Before you begin, answer the questions as A/a, B/b, C/c, D/c.\n EX. "A"')
+
+begin = input("Start? Y/N ")
+
+## QUIZ QUESTIONS ##
+
+questionone = input("\nA. Expansion of territory\nB. Champa rice\nC. Industrialization\nD. Religious reforms")
+
+def questiononePrompt():
+    print("1. What was the main reason for the\nSong Dynasty’s population growth?\n")
+
+    questionOne
+    
+if begin == "y" or "Y":
+    questionone
